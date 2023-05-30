a7a627a3738b4c20ec1bc5029d0d5f6a0aa24b2e
Umais Leghari <leghari221@gmail.com>
2023-05-30 15:33:30 +0500

    Update hamarai.py

--- hamarai.py
+++ hamarai.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib
-matplotlib.use("Agg")
 import matplotlib.pyplot as plt
+import seaborn as sns
 import seaborn as sns
 
 # Load the Iris dataset
